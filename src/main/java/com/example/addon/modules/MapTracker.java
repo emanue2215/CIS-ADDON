6bb237484f93a2b506db23f8273d543a8ef0ab25
emanue2215 <98289014+emanue2215@users.noreply.github.com>
2025-07-13 12:23:16 -0400

    MapTracker.java

--- src/main/java/com/example/addon/modules/MapTracker.java
+++ src/main/java/com/example/addon/modules/MapTracker.java
@@ -5,13 +5,14 @@ import meteordevelopment.meteorclient.utils.player.ChatUtils;
 import net.minecraft.item.FilledMapItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.map.MapState;
+import net.minecraft.item.map.MapDecoration;
+import net.minecraft.item.map.MapBannerMarker;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 import java.util.Map;
-import java.util.UUID;
 
 public class MapTracker extends Module {
     public MapTracker() {
@@ -54,33 +55,35 @@ public class MapTracker extends Module {
         Identifier dimensionId = dimensionKey.getValue();
         ChatUtils.info("Dimension: " + dimensionId);
         ChatUtils.info("Locked: " + state.locked);
-        ChatUtils.info("Map ID: " + FilledMapItem.getMapId(item));
+        ChatUtils.info("Map ID: " + FilledMapItem.getMapId(item.getNbt()));
 
-        if (!state.icons.isEmpty()) {
+        if (!state.decorations.isEmpty()) {
             ChatUtils.info("Icons:");
-            for (Map.Entry<UUID, MapState.Icon> entry : state.icons.entrySet()) {
-                MapState.Icon icon = entry.getValue();
-                ChatUtils.info(" - Type: " + icon.type.asString() + ", X = " + icon.getX() + ", Z = " + icon.getZ());
+            for (Map.Entry<String, MapDecoration> entry : state.decorations.entrySet()) {
+                MapDecoration icon = entry.getValue();
+                ChatUtils.info(" - Type: " + icon.getType().name() + ", X = " + icon.getX() + ", Z = " + icon.getZ());
             }
         } else {
             ChatUtils.info("Icons: None");
         }
 
-        if (!state.banners.isEmpty()) {
+        if (!state.getBanners().isEmpty()) {
             ChatUtils.info("Banners:");
-            for (Map.Entry<BlockPos, String> banner : state.banners.entrySet()) {
-                ChatUtils.info(" - " + banner.getValue() + " at " + banner.getKey().toShortString());
+            for (Map.Entry<String, MapBannerMarker> banner : state.getBanners().entrySet()) {
+                MapBannerMarker marker = banner.getValue();
+                BlockPos pos = marker.getPos();
+                ChatUtils.info(" - Banner at " + pos.toShortString());
             }
         } else {
             ChatUtils.info("Banners: None");
         }
 
-        if (state.framePos != null) {
-            ChatUtils.info("Item Frame Position: " + state.framePos.toShortString());
+        if (state.getFramePos() != null) {
+            ChatUtils.info("Item Frame Position: " + state.getFramePos().toShortString());
         } else {
             ChatUtils.info("Item Frame Position: None");
         }
 
-        toggle(); // Disable module after showing info
+        toggle();
     }
 }
