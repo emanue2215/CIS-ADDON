8723dd1633bf7772104987039fbf7fe5305e78a6
emanue2215 <98289014+emanue2215@users.noreply.github.com>
2025-07-12 21:59:36 -0400

    HudExample.java

--- src/main/java/com/example/addon/hud/HudExample.java
+++ src/main/java/com/example/addon/hud/HudExample.java
@@ -1,6 +1,6 @@
-package com.example.addon.hud;
+package com.zorrilo197.cisaddon.hud;
 
-import com.example.addon.AddonTemplate;
+import com.example.addon.CISAddon;
 import meteordevelopment.meteorclient.systems.hud.HudElement;
 import meteordevelopment.meteorclient.systems.hud.HudElementInfo;
 import meteordevelopment.meteorclient.systems.hud.HudRenderer;
